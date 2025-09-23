0ecef4f18d1efbfb950e6d034149d453865edf81
Gregory Veloso <gregory.v.silva@aluno.senai.br>
2025-09-23 18:37:30 -0300

    crud usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,5 +13,25 @@ roteador.get("/login", userController.formLogin)
 //Rota pra enviar dados na página de login
 roteador.post("/login", userController.loginUsuario)
 
+
+// CRUD
+//C = CRIAR NOVO USUÁRIO
+//Rota pra solicitar a página de cadastro
+roteador.get("/cadastrar", userController.formCadastro)
+//Rota pra enviar dados de cadastro
+roteador.post("/cadastrar", userController.salvarUsuario)
+
+//R = OBTER INFORMAÇÕES DE USUÁRIOS
+// Retorna as informações de todos os usuarios
+roteador.get("/", userController.listarUsuarios)
+// Retorna as informações de um usuário apenas
+roteador.get("/:id", userController.buscarUsuario)
+
+// U = ATUALIZAR UM USUÁRIO
+roteador.put("/:id", userController.atualizarUsuario)
+
+//D = DELETAR UM USUÁRIO
+roteador.delete("/:id", userController.deletarUsuario)
+
 // Criando a exportação desse arquivo
 module.exports = roteador
